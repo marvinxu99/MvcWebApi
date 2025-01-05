45b67966df419635a3396b14f4f1f1e7e213abde
Marvin Xu <marvinxu99@hotmail.com>
2025-01-05 10:51:49 -0800

    ...

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,34 +1,49 @@
 
-namespace ToDoApi
+// https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-9.0&tabs=visual-studio
+
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddDbContext<TodoContext>(opt =>
+            opt.UseInMemoryDatabase("ToDoList"));
+
+        //builder.Services.AddEndpointsApiExplorer();
+        //builder.Services.AddSwaggerGen();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
+            app.MapOpenApi();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi("internal");
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            //app.UseSwagger();
+            //app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        //app.MapGet("/", () => "Hello World!");
+
+        app.Run();
     }
 }
